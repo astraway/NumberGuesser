19ddcde2de5f12c25ef0d75e4ff0df47f6ab20a9
Eric Jurotich <ejurotic@cisco.com>
2019-10-03 15:08:58 -0700

    Removed a comment

--- NumberGuesser/Program.cs
+++ NumberGuesser/Program.cs
@@ -8,7 +8,7 @@ namespace NumberGuesser
         {
             string name = "Eric";
             int age = 37;
-            //Console.WriteLine("Hello " + name + ", you are " + age + " years old");
+            
             Console.WriteLine("{0} is {1}", name, age);
                 
         }
