bd9b7e2016c04ace614e9a67f16d636c2ebd92fa
Eric Jurotich <ejurotic@cisco.com>
2019-10-03 15:12:58 -0700

    Added app Vars

--- NumberGuesser/Program.cs
+++ NumberGuesser/Program.cs
@@ -6,10 +6,12 @@ namespace NumberGuesser
     {
         static void Main(string[] args)
         {
-            string name = "Eric";
-            int age = 37;
-            
-            Console.WriteLine("{0} is {1}", name, age);
+            //Set app vars
+            string appName = "Number Guesser";
+            string appVersion = "1.0.0";
+            string appAuther = "Eric Jurotich";
+
+            Console.WriteLine("{0}: Version {1} by {2}", appName, appVersion, appAuther);
                 
         }
     }
