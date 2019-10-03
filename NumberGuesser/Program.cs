505da44404df43f92ec80430ccf6fbbba2a84f2d
Eric Jurotich <ejurotic@cisco.com>
2019-10-03 14:47:39 -0700

    Add project files.

---
+++ NumberGuesser/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+// Namespace
+namespace NumberGuesser
+{   // Main class
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Eric";
+            int age = 37;
+            //Console.WriteLine("Hello " + name + ", you are " + age + " years old");
+            Console.WriteLine("{0} is {1}", name, age);
+                
+        }
+    }
+}
